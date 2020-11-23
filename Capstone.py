1587ece0ca6fcbac8b3052bbc15f3c757eaa787f
sudharshan-chakrapani <45945687+sudharshan-chakrapani@users.noreply.github.com>
2020-11-23 17:45:00 +0100

    Add files via upload

---
+++ Capstone.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#This is the capstone project for Applied data science level of IBM professional certificate. 
+import pandas as pd
+import numpy as np
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
